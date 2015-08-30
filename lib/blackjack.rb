0c227416a6fe1cf10b15bdd06ed92f34b7b2db69
mirc89 <kmircovich1@gmail.com>
2015-08-29 21:42:48 -0400

    Game is working, some tests are not passing

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,61 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(11)+1 #How to account for Aces later?..
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(current_card_total)
+  puts "Your cards add up to #{current_card_total.to_s}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp.downcase
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  @hand = deal_card + deal_card
+  display_card_total(@hand)
+  return @hand
 end
 
 def hit?
-  # code hit? here
+  #couldn't get these tests passing so I made code that would still make the game work.
+  i = 1
+  while i == 1
+    prompt_user
+    action = get_user_input
+    if action == "s"
+      i = 0
+    elsif action == "h"
+      @hand = deal_card + @hand
+        if @hand == 21 
+          success_message
+          break
+        elsif @hand > 21
+          end_game(@hand)
+          break
+        end
+      display_card_total(@hand)
+    else
+      invalid_command
+    end
+  end
+  return @hand
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Sorry, that is an invalid command."
+  # sleep(1) #here to be more user friendly in the command line.
 end
 
 #####################################################
@@ -40,5 +64,35 @@ end
 
 def runner
   # code runner here
+  welcome
+  initial_round
+  check_initial_hand
+  hit?
 end
-    
+
+#####################################################
+# additional methods
+#####################################################
+
+def success_message
+  puts "Nice! You hit 21!"
+end
+
+def check_for_aces
+  #calculates whether to count an ace as high or low
+end
+
+def check_initial_hand #this is used for when initial_round returns 21/22
+  if @hand == 21
+    success_message
+    return @hand
+  elsif @hand == 22
+    end_game(@hand)
+    #later to be changed to:
+    #@hand = 12
+    #but need re-write the program to give user option to place aces high or low
+  end
+end
+
+
+
